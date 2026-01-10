4f1bdd00ee6e4a518a3cd67b7f19d7e49be5d369
Nokib Sarkar <nokibsarkar@gmail.com>
2026-01-10 14:16:47 +0000

    Refactor session management: integrate API error handling and improve loading/error states

--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -2,34 +2,49 @@ import { useEffect, useState } from "react";
 import type { Session } from "../types/session";
 import sessionContext from "../contexts/SessionContext";
 import { Navigate } from "react-router-dom";
-
+import { fetchAPIFromBackendSingleWithErrorHandling } from "@/api";
+const SessionLoading = () => {
+    return <div>Loading who are you...</div>;
+}
+const SessionError = ({ error }: { error: Error }) => {
+    return <div>Error loading session: {error.message}</div>;
+}
 const SessionProvider = ({ children }: { children: React.ReactNode }) => {
+    const [sessionLoading, setSessionLoading] = useState(true);
     const [session, setSession] = useState<Session | null>(null);
+    const [sessionError, setSessionError] = useState<Error | null>(null);
     useEffect(() => {
         const fetchSession = async () => {
+            setSessionLoading(true);
+            setSessionError(null);
             try {
-                const response = await fetch('/api/session', {
-                    credentials: 'include',
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    setSession(data as Session);
-                } else {
-                    setSession(null);
+                const response = await fetchAPIFromBackendSingleWithErrorHandling<Session>('/user/me');
+                if ('detail' in response) {
+                    throw new Error(response.detail);
                 }
+                setSession(response.data);
             } catch (error) {
                 setSession(null);
                 console.error("Failed to fetch session:", error);
+                setSessionError(error as Error);
+            } finally {
+                setSessionLoading(false);
             }
         };
         fetchSession();
     }, []);
-    if(!session) {
+    if (!session && !sessionLoading) {
         let path = encodeURIComponent(window.location.pathname + window.location.search);
-        if(window.location.pathname.startsWith('/user/login')) {
+        if (window.location.pathname.startsWith('/user/login')) {
             path = '/';
         }
-        return <Navigate to={`/user/login?redirect=${path}`} replace />;
+        return <Navigate to={`/user/login?next=${path}`} replace />;
+    }
+    if (sessionLoading) {
+        return <SessionLoading />;
+    }
+    if (sessionError) {
+        return <SessionError error={sessionError} />;
     }
     return (
         <sessionContext.Provider value={session}>
