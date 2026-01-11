38fe66c2a9ac19121c30ca5a031deecd12374759
Nokib Sarkar <nokibsarkar@gmail.com>
2026-01-11 18:14:33 +0000

    Add LottieWrapper component and integrate dotlottie-react for animation support

---
+++ src/components/LottieWrapper.tsx
@@ -0,0 +1,11 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react"
+const LottieWrapper = ({ src, loop = true, marginTop }: { src: string, loop?: boolean, marginTop?: string }) => (
+    <div style={{ maxWidth: '500px', margin: 'auto', width: '99%', marginTop: marginTop }}>
+        <DotLottieReact
+            src={src}
+            loop={loop}
+            autoplay
+        />
+    </div>
+)
+export default LottieWrapper
